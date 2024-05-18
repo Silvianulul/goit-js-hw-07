9c4936d0de2113f34b53768be780be81e6eeb911
Silvianulul <silvi_mitrica@yahoo.com>
2024-05-18 18:48:13 +0300

    js-hw-07

---
+++ js/02-lightbox.js
@@ -0,0 +1,35 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+      `;
+    })
+    .join("");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+});
